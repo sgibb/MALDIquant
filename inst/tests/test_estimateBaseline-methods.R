25b77b77af7a2e2115cbc65e633d7d4acaa55638
Sebastian Gibb <mail@sebastiangibb.de>
2013-01-30 20:01:52 +0100

    add estimateNoise tests

--- inst/tests/test_estimateBaseline-methods.R
+++ inst/tests/test_estimateBaseline-methods.R
@@ -19,7 +19,6 @@ test_that("estimateBaseline shows warnings", {
 test_that("estimateBaselineConvexHull", {
   m[, 2] <- c(10:1, rep(1, 10))
   ## C implementation
-  print(MALDIquant:::.estimateBaselineConvexHull(1:20, i), m)
   expect_identical(MALDIquant:::.estimateBaselineConvexHull(1:20, i), m)
   ## obsolete (slow) R implementation
   expect_identical(MALDIquant:::.lowerConvexHullR(1:20, i), m)
