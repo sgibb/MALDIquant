696ac55b83f70e34173bae26ec123c3a36d5f35b
Sebastian Gibb <mail@sebastiangibb.de>
2013-02-22 18:09:45 +0100

    Revert "replace localMaxima implementation, using dilation filter instead of own "algorithm""
    This reverts commit 2c08217e5e452300817c9163cf9a1183840c8fb1.

--- src/localMaxima.c
+++ src/localMaxima.c
@@ -1,4 +1,4 @@
-/* Copyright 2012-2013 Sebastian Gibb
+/* Copyright 2012 Sebastian Gibb
  * <mail@sebastiangibb.de>
  *
  * This file is part of MALDIquant for R and related languages.
@@ -19,65 +19,49 @@
 
 #include <R.h>
 
-/* based on vHGW dilation Algorithm, see morphological_filters.h for details */
-
-/* f = input vector
- * fn = length of f
- * n = number of values in f before padding of zeros left and right
- * k = filter length
- * q = floor(k/2)
- * output = output vector
- */
-void localMaxima(double* f, int fn, int n, int k, int q, int* output) {
-  /* allocate vector - error handling is done by R */
-  double* g=(double*) Calloc((size_t) fn, double);
-  double* h=(double*) Calloc((size_t) fn, double);
-
-  unsigned int i, r, j, fi, gi, hi;
-
-  /* init extrema */
-  r=q+n-1;
-  for (i=0, gi=q+n+q-1, hi=0;  i<q; ++i, --gi, ++hi) {
-    g[gi]=f[r];
-    h[hi]=f[q];
-  }
-
-  /* preprocessing */
-  for (i=q, r=i+k-1; i<n+q; i+=k, r+=k) {
-
-    /* init most left/right elements */
-    g[i]=f[i];
-    h[r]=f[r];
-
-    for (j=1, gi=i+1, hi=r-1; j<k; ++j, ++gi, --hi) {
-      if (g[gi-1] < f[gi]) {
-        g[gi]=f[gi];
-      } else {
-        g[gi]=g[gi-1];
-      }
-      if (h[hi+1] < f[hi]) {
-        h[hi]=f[hi];
-      } else {
-        h[hi]=h[hi+1];
-      }
-    }
-
-  }
-
-  /* merging */
-  for (i=0, fi=q, gi=k-1, hi=0; i<n; ++i, ++fi, ++gi, ++hi) {
-    if (g[gi] < h[hi]) {
-      output[i]=h[hi]==f[fi];
-    } else {
-      output[i]=g[gi]==f[fi];
+int windowMaxIdx(double* x, int start, int end) {
+  int m=start;
+  for (int i=start+1; i<=end; ++i) {
+    if (x[m] < x[i]) {
+      m=i;
     }
   }
-
-  Free(g);
-  Free(h);
+  return(m);
 }
 
-void R_localMaxima(double* f, int* fn, int* n, int* k,  int* q, int* output) {
-  localMaxima(f, *fn, *n, *k, *q, output);
+int middle(int pos, int start, int end) {
+  return(pos == (start + end)/2);
+}
+
+/* y = array of double values
+ * length = length of y
+ * span = half window size
+ */
+void R_localMaxima(double* y, int* length, int* span, int* output) {
+    
+    int n=*length;
+    int windowSize=*span*2;
+
+    int m=windowMaxIdx(y, 0, windowSize);
+
+    output[m]=middle(m, 0, windowSize);
+
+    int l=0;
+
+    for (int i=windowSize+1; i<n; ++i) {
+      /* i == rhs; l == lhs */
+      l=i-windowSize;
+      /* maximum out of window,
+       * calculate new maximum in current window */
+      if (m < l) {
+        m=windowMaxIdx(y, l, i);
+      } else if (y[i] > y[m]) {
+        m=i;
+      }
+      
+      if (middle(m, l, i)) {
+        output[m]=1;
+      }
+    }
 }
 
