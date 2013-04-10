1f00df689e7441e13ec9d15699731f7309b6fe8e
Sebastian Gibb <mail@sebastiangibb.de>
2013-04-10 18:46:04 +0200

    add test case for "erosion right extrema bug"

--- inst/tests/test_morphologicalFilter-functions.R
+++ inst/tests/test_morphologicalFilter-functions.R
@@ -1,14 +1,23 @@
 context("morphological filters")
 
-x <- c(15, 8, 4, 11, 10, 1, 13, 12, 6, 9, 5, 3, 2, 7, 14)
+x <- list(c(8, 6, 2, 4, 5, 7, 1, 10),
+          c(15, 8, 4, 11, 10, 1, 13, 12, 6, 9, 5, 3, 2, 7, 14))
+
+e <- list(c(rep(2:1, each=4)),
+          c(rep(4, 3), rep(1, 5), 5, 3, rep(2, 5)))
+
+d <- list(c(rep(8, 3), rep(7, 2), rep(10, 3)),
+          c(rep(15, 3), 11, rep(13, 5), 12, rep(9, 2), rep(14, 3)))
 
 test_that(".erosion", {
-  expect_identical(.erosion(x, halfWindowSize=2),
-                   c(rep(4, 3), rep(1, 5), 5, 3, rep(2, 5)))
+  for (i in seq(along=x)) {
+    expect_equal(.erosion(x[[i]], halfWindowSize=2), e[[i]])
+  }
 })
 
 test_that(".dilation", {
-  expect_identical(.dilation(x, halfWindowSize=2),
-                   c(rep(15, 3), 11, rep(13, 5), 12, rep(9, 2), rep(14, 3)))
+  for (i in seq(along=x)) {
+    expect_equal(.dilation(x[[i]], halfWindowSize=2), d[[i]])
+  }
 })
 
