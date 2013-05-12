d10d6886b8d5e2fb17391b4d8729748684e44a84
Sebastian Gibb <mail@sebastiangibb.de>
2013-05-12 10:09:06 +0200

    add test cases for integer overflow (#25)

--- inst/tests/test_isEmpty-methods.R
+++ inst/tests/test_isEmpty-methods.R
@@ -1,6 +1,7 @@
 context("isEmpty")
 
-s <- createMassSpectrum(mass=1:10, intensity=11:20)
+s <- c(createMassSpectrum(mass=1:10, intensity=11:20),
+       createMassSpectrum(mass=1:2, intensity=rep.int(.Machine$integer.max, 2)))
 e <- c(createMassSpectrum(mass=double(), intensity=double()),
        createMassSpectrum(mass=1:10, intensity=rep(0, 10)),
        createMassSpectrum(mass=1:10, intensity=rep(NA, 10)))
@@ -9,12 +10,14 @@ test_that("isEmpty", {
   for (i in seq(along=e)) {
     expect_true(isEmpty(e[[i]]))
   }
-  expect_false(isEmpty(s))
+  for (i in seq(along=s)) {
+    expect_false(isEmpty(s[[i]]))
+  }
 })
 
 test_that("isEmptyWarning", {
   expect_warning(MALDIquant:::.isEmptyWarning(e[[1]]), "empty")
   expect_true(suppressWarnings(MALDIquant:::.isEmptyWarning(e[[1]])))
-  expect_false(MALDIquant:::.isEmptyWarning(s))
+  expect_false(MALDIquant:::.isEmptyWarning(s[[1]]))
 })
 
