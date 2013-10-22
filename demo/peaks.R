fc1ded2b6f7b15097e54be4a94d3afb5b963a435
Sebastian Gibb <mail@sebastiangibb.de>
2013-10-22 20:50:55 +0200

    extend peaks demo

--- demo/peaks.R
+++ demo/peaks.R
@@ -12,24 +12,56 @@ library("MALDIquant")
 data("fiedler2009subset", package="MALDIquant")
 
 ## choose only spectrum 1
-s <- fiedler2009subset[[1]]
+s1 <- fiedler2009subset[[1]]
+
+
+## preprocessing
+## sqrt transform (for variance stabilization)
+s2 <- transformIntensity(s1, method="sqrt")
+
+
+## 21 point Savitzky-Golay-Filter for smoothing spectra
+## (maybe you have to adjust the halfWindowSize;
+## you could use a simple moving average instead)
+## see ?smoothIntensity, ?.savitzkyGolay, ?.movingAverage
+s3 <- smoothIntensity(s2, method="SavitzkyGolay", halfWindowSize=10)
+
 
 ## remove baseline
-s <- removeBaseline(s)
+## (maybe you have to adjust iterations to your spectra; high resolution
+## spectra need a much lower iteration number (halfWindowSize, for some other
+## baseline estimation algorithms)
+## see ?removeBaseline, ?estimateBaseline
+s4 <- removeBaseline(s3, method="SNIP", iterations=100)
 
-## detect peaks
-p <- detectPeaks(s, SNR=10)
 
-## plot spectrum
-plot(s, main="example spectrum 1")
+## run peak detection
+## (maybe you need to adjust halfWindowSize [decreasing it for high resolution
+## spectra] and SNR [a higher value increase the True-Positive-Rate but decrease
+## sensitivity])
+## see ?detectPeaks, ?estimateNoise
+p <- detectPeaks(s4, method="MAD", halfWindowSize=20, SNR=2)
 
-## draw vertical lines
-lines(p, col=4)
 
-## mark peaks
-points(p, col=2, pch=4)
+## produce some plots
+par(mfrow=c(2,3))
 
-## label some peaks
-labelPeaks(p, mass=c(1020, 1206, 1350, 1466, 1616, 2660, 2932, 3191, 3262,
-                     3883, 4209, 5336, 5904, 7766, 9290))
+xlim <- range(mass(s1)) # use same xlim on all plots for better comparison
 
+plot(s1, main="1: raw", sub="", xlim=xlim)
+plot(s2, main="2: variance stabilization", sub="", xlim=xlim)
+plot(s3, main="3: smoothing", sub="", xlim=xlim)
+plot(s4, main="4: baseline correction", sub="", xlim=xlim)
+
+plot(s4, main="5: peak detection", sub="", xlim=xlim)
+points(p)
+
+
+## label top 20 peaks
+top20 <- intensity(p) %in% sort(intensity(p), decreasing=TRUE)[1:20]
+labelPeaks(p, index=top20, underline=TRUE)
+
+plot(p, main="6: peak plot", sub="", xlim=xlim)
+labelPeaks(p, index=top20, underline=TRUE)
+
+par(mfrow=c(1,1))
