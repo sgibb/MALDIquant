c287058ab3e766134b349c0c7a53e36f121d86a8
Sebastian Gibb <mail@sebastiangibb.de>
2015-01-24 18:24:51 +0100

    add test for empty spectra with file path

--- tests/testthat/test_isEmpty-methods.R
+++ tests/testthat/test_isEmpty-methods.R
@@ -2,7 +2,8 @@ context("isEmpty")
 
 s <- c(createMassSpectrum(mass=1:10, intensity=11:20),
        createMassSpectrum(mass=1:2, intensity=rep.int(.Machine$integer.max, 2)))
-e <- c(createMassSpectrum(mass=double(), intensity=double()),
+e <- c(createMassSpectrum(mass=double(), intensity=double(),
+                          metaData=list(file="fid")),
        createMassSpectrum(mass=1:10, intensity=rep(0, 10)),
        createMassSpectrum(mass=1:10, intensity=as.double(rep(NA, 10))))
 
@@ -16,7 +17,10 @@ test_that("isEmpty", {
 })
 
 test_that("isEmptyWarning", {
-  expect_warning(MALDIquant:::.isEmptyWarning(e[[1]]), "empty")
+  expect_warning(MALDIquant:::.isEmptyWarning(e[[1]]),
+                 "MassSpectrum object \\(file: fid\\) is empty")
+  expect_warning(MALDIquant:::.isEmptyWarning(e[[2]]),
+                 "MassSpectrum object is empty")
   expect_true(suppressWarnings(MALDIquant:::.isEmptyWarning(e[[1]])))
   expect_false(MALDIquant:::.isEmptyWarning(s[[1]]))
 })
