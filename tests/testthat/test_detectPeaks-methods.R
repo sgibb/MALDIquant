ad692d2c1cb187ac6ef2b5f57ebd852964e06885
Sebastian Gibb <mail@sebastiangibb.de>
2015-01-24 19:01:15 +0100

    add test case for detectPeaks method

--- tests/testthat/test_detectPeaks-methods.R
+++ tests/testthat/test_detectPeaks-methods.R
@@ -1,6 +1,11 @@
 context("detectPeaks")
 
 s <- createMassSpectrum(mass=1:5, intensity=c(1, 2, 1, 2, 1))
+p <- createMassPeaks(c(2, 4), c(2, 2), c(Inf, Inf))
+
+test_that("detectPeaks", {
+  expect_equal(detectPeaks(s, halfWindowSize=1), p)
+})
 
 test_that("detectPeaks shows warnings", {
   expect_warning(detectPeaks(createMassSpectrum(mass=double(),
@@ -12,6 +17,6 @@ test_that("detectPeaks works with list of MassSpectrum objects", {
                "no list of MALDIquant::MassSpectrum objects")
   expect_error(detectPeaks(list(s, createMassPeaks(1, 1)),
                "no list of MALDIquant::MassSpectrum objects"))
-  p <- createMassPeaks(c(2, 4), c(2, 2), c(Inf, Inf))
+  expect_equal(detectPeaks(list(s, s), halfWindowSize=1), list(p, p))
 })
 
