efcc18def63a6adc5568300c5be36644f26ffb96
Sebastian Gibb <mail@sebastiangibb.de>
2015-06-08 21:41:49 +0200

    rewrite show unit tests to successed on Windows i386

--- tests/testthat/test_show-methods.R
+++ tests/testthat/test_show-methods.R
@@ -3,56 +3,80 @@ context("show")
 test_that("show", {
   x <- createMassSpectrum(mass=1:10, intensity=11:20,
                           metaData=list(name="example", file="example.mzML"))
-  r <- c(
-   "S4 class type            : MassSpectrum",
-   "Number of m/z values     : 10          ",
-   "Range of m/z values      : 1 - 10      ",
-   "Range of intensity values: 11 - 20     ",
-   paste("Memory usage             :", MALDIquant:::.memoryUsageStr(x), "  "),
-   "Name                     : example     ",
-   "File                     : example.mzML")
+  r <- paste0(format(c("S4 class type",
+                       "Number of m/z values",
+                       "Range of m/z values",
+                       "Range of intensity values",
+                       "Memory usage",
+                       "Name",
+                       "File"), justify="left"), ": ",
+              c(format(c("MassSpectrum",
+                         "10",
+                         "1 - 10",
+                         "11 - 20",
+                         MALDIquant:::.memoryUsageStr(x),
+                         "example"), justify="left"),
+                "example.mzML"))
 
-  expect_identical(capture.output(show(x)), r)
+  expect_identical(capture.output(x), r)
 
   x <- createMassPeaks(mass=1:10, intensity=11:20,
                        metaData=list(name="example"))
+  r <- paste0(format(c("S4 class type",
+                       "Number of m/z values",
+                       "Range of m/z values",
+                       "Range of intensity values",
+                       "Range of snr values",
+                       "Memory usage",
+                       "Name"), justify="left"), ": ",
+              format(c("MassPeaks",
+                       "10",
+                       "1 - 10",
+                       "11 - 20",
+                       "NA - NA",
+                       MALDIquant:::.memoryUsageStr(x),
+                       "example"), justify="left"))
 
-  r <- c(
-   "S4 class type            : MassPeaks",
-   "Number of m/z values     : 10       ",
-   "Range of m/z values      : 1 - 10   ",
-   "Range of intensity values: 11 - 20  ",
-   "Range of snr values      : NA - NA  ",
-   paste("Memory usage             :", MALDIquant:::.memoryUsageStr(x)),
-   "Name                     : example  ")
-
-  expect_identical(capture.output(show(x)), r)
+  expect_identical(capture.output(x), r)
 
   x <- createMassPeaks(mass=1:10, intensity=11:20, snr=1:10,
                        metaData=list(name=c("example1", "example2"),
                                      file=c("example1.txt", "example2.txt")))
-  r <- c(
-   "S4 class type            : MassPeaks",
-   "Number of m/z values     : 10       ",
-   "Range of m/z values      : 1 - 10   ",
-   "Range of intensity values: 11 - 20  ",
-   "Range of snr values      : 1 - 10   ",
-   paste("Memory usage             :", MALDIquant:::.memoryUsageStr(x)),
-   "Name1                    : example1 ",
-   "Name2                    : example2 ",
-   "File1                    : example1.txt",
-   "File2                    : example2.txt")
+  r <- paste0(format(c("S4 class type",
+                       "Number of m/z values",
+                       "Range of m/z values",
+                       "Range of intensity values",
+                       "Range of snr values",
+                       "Memory usage",
+                       "Name1",
+                       "Name2",
+                       "File1",
+                       "File2"), justify="left"), ": ",
+              c(format(c("MassPeaks",
+                         "10",
+                         "1 - 10",
+                         "11 - 20",
+                         "1 - 10",
+                         MALDIquant:::.memoryUsageStr(x),
+                         "example1",
+                         "example2"), justify="left"),
+                "example1.txt", "example2.txt"))
 
-  expect_identical(capture.output(show(x)), r)
+  expect_identical(capture.output(x), r)
 
   x <- createMassPeaks(double(), double())
-  r <- c(
-   "S4 class type            : MassPeaks",
-   "Number of m/z values     : 0        ",
-   "Range of m/z values      : NA       ",
-   "Range of intensity values: NA       ",
-   "Range of snr values      : NA       ",
-   paste0("Memory usage             : ", MALDIquant:::.memoryUsageStr(x), " "))
+  r <- paste0(format(c("S4 class type",
+                       "Number of m/z values",
+                       "Range of m/z values",
+                       "Range of intensity values",
+                       "Range of snr values",
+                       "Memory usage"), justify="left"), ": ",
+              format(c("MassPeaks",
+                       "0",
+                       "NA",
+                       "NA",
+                       "NA",
+                       MALDIquant:::.memoryUsageStr(x)), justify="left"))
 
-  expect_identical(capture.output(show(x)), r)
+  expect_identical(capture.output(x), r)
 })
