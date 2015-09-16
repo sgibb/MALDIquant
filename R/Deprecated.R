0642a43a8538db2e36659365fec54997da7dd779
Sebastian Gibb <mail@sebastiangibb.de>
2015-09-16 11:20:01 +0200

    remove plotImsSlice (was defunct since 1.13)

--- R/Deprecated.R
+++ R/Deprecated.R
@@ -1,10 +1,1 @@
-## deprecated since MALDIquant 1.11.12
-plotImsSlice <- function(x, range=c(0, Inf),
-                         sub=paste0("m/z: ", range[1L], "-", range[2L], ""),
-                         removeEmptyRows=TRUE,
-                         removeEmptyCols=TRUE,
-                         colRamp=colorRamp(c("black", "blue", "green",
-                                             "yellow", "red")),
-                         interpolate=FALSE, ...) {
-  .deprecatedFunction("1.11.12", new="plotMsiSlice")
-}
+## deprecated since MALDIquant x.y.z
