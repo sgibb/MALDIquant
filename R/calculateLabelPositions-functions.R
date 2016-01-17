60273b8b31d9d419bf7cdc8b24df2360a2082d47
Sebastian Gibb <mail@sebastiangibb.de>
2016-01-17 14:02:22 +0100

    remove textLabelRect-functions.R and move it content to calculateLabelPositions

--- R/calculateLabelPositions-functions.R
+++ R/calculateLabelPositions-functions.R
@@ -136,3 +136,32 @@
 
   textOverlap
 }
+
+## .textLabelRects
+##  calculate label rectangles
+##
+## params:
+##  x: original x coordinates for labels
+##  y: original y coordinates for labels
+##  text: peak labels
+##  adj: text alignment ?par("adj")
+##  cex: font size ?par("cex")
+##  offset: offset
+##
+## returns:
+##  a matrix of coordinates
+##
+.textLabelRects <- function(x, y, text, adj, cex, offset=c(0.0, 0.2)) {
+  t(mapply(function(xc, yc, t) {
+    w <- strwidth(t, cex=cex)
+    h <- strheight(t, cex=cex)
+    ## extra calculation of offsets to be independend of adj
+    woffset <- w * offset[1L]
+    hoffset <- h * offset[2L]
+    return(c(x0=xc - w * adj[1L] - woffset,
+             y0=yc - h * adj[2L] - hoffset,
+             x1=xc + w * (1L - adj[1L]) + woffset,
+             y1=yc + h * (1L - adj[2L]) + hoffset,
+             x=xc, y=yc, w=w, h=h))
+  }, xc=x, yc=y, t=text))
+}
