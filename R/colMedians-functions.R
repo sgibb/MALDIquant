c4ada489d9370203bd2bfc5dab770343f8c713c2
Sebastian Gibb <mail@sebastiangibb.de>
2016-03-13 18:15:28 +0100

    add .colMaxs and .colCors

--- R/colMedians-functions.R
+++ R/colMedians-functions.R
@@ -1,4 +1,4 @@
-## Copyright 2013 Sebastian Gibb
+## Copyright 2013-2016 Sebastian Gibb
 ## <mail@sebastiangibb.de>
 ##
 ## This file is part of MALDIquant for R and related languages.
@@ -19,4 +19,45 @@
 .colMedians <- function(x, na.rm=FALSE) {
   stopifnot(is.matrix(x), is.logical(na.rm))
   .Call("C_colMedians", x, na.rm)
+}
+
+#' .colMaxs
+#'
+#' Similar to apply(x, 2, max).
+#'
+#' @param x matrix/data.frame
+#' @return double
+#' @author Sebastian Gibb <mail@@sebastiangibb.de>
+#' @noRd
+.colMaxs <- function(x) {
+  x[max.col(t(x), ties.method="first") + 0L:(ncol(x) - 1L) * nrow(x)]
+}
+
+#' .colCors
+#'
+#' Calculate the correlation for two matrices columnwise.
+#'
+#' @param x matrix/data.frame
+#' @return double
+#' @author Sebastian Gibb <mail@@sebastiangibb.de>
+#' @noRd
+.colCors <- function(x, y, na.rm=FALSE) {
+  stopifnot(is.matrix(x) && is.matrix(y))
+  stopifnot(all(dim(x) == dim(y)))
+
+  if (na.rm) {
+    isNA <- is.na(x) | is.na(y)
+    n <- colSums(!isNA)
+    x[isNA] <- NA_real_
+    y[isNA] <- NA_real_
+  } else {
+    n <- nrow(x)
+  }
+
+  cmX <- colMeans(x, na.rm=na.rm)
+  cmY <- colMeans(y, na.rm=na.rm)
+
+  (colMeans(x * y, na.rm=na.rm) - (cmX*cmY)) /
+    (sqrt(colMeans(x * x, na.rm=na.rm) - cmX*cmX) *
+     sqrt(colMeans(y * y, na.rm=na.rm) - cmY*cmY))
 }
--- tests/testthat/test_colMedians-functions.R
+++ tests/testthat/test_colMedians-functions.R
@@ -21,3 +21,33 @@ test_that(".colMedians", {
   expect_equal(MALDIquant:::.colMedians(m), colMedians(m))
   expect_equal(MALDIquant:::.colMedians(m, TRUE), colMedians(m, TRUE))
 })
+
+test_that(".colMaxs", {
+  colMaxs <- function(x)apply(x, 2, max)
+  set.seed(1234)
+  ## even nrow
+  m <- matrix(rnorm(1e5), ncol=1e2)
+  expect_equal(MALDIquant:::.colMaxs(m), colMaxs(m))
+})
+
+test_that(".colCors", {
+  colCors <- function(x, y, use="everything") {
+    z <- double(ncol(x))
+    for (i in 1:ncol(x)) {
+      z[i] <- stats::cor(x[,i], y[,i], use=use)
+    }
+    z
+  }
+  set.seed(1234)
+  m <- matrix(rnorm(1e5), ncol=1e2)
+  n <- matrix(rnorm(1e5), ncol=1e2)
+  mna <- m
+  nna <- n
+  mna[sample(1e5, 1e3)] <- NA
+  nna[sample(1e5, 1e3)] <- NA
+  expect_equal(MALDIquant:::.colCors(m, n), colCors(m, n))
+  expect_equal(MALDIquant:::.colCors(mna, nna), colCors(mna, nna))
+  expect_equal(MALDIquant:::.colCors(mna, nna, na.rm=TRUE),
+               colCors(mna, nna, use="na.or.complete"))
+})
+
