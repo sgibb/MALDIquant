bcc58583ff25be53dd3850ba7e833bbd6ac86028
Sebastian Gibb <mail@sebastiangibb.de>
2016-12-02 19:26:18 +0100

    add tolerance and nomatch argument to .which.closest

--- R/which.closest-functions.R
+++ R/which.closest-functions.R
@@ -22,23 +22,29 @@
 ## params:
 ##  x: numeric key value to look for
 ##  vec: numeric, has to be sorted
+##  tolerance: accepted tolerance; Inf to choose the closest (old behaviour)
+##  nomatch: if tolerance !Inf and the difference is larger than tolerance
+##  return nomatch.
 ##
 ## returns:
 ##  a vector of indices
 ##
-.which.closest <- function(x, vec) {
+.which.closest <- function(x, vec, tolerance=Inf, nomatch=NA_integer_) {
 
   ## find left interval
   lIdx <- findInterval(x, vec, rightmost.closed=FALSE, all.inside=TRUE)
   rIdx <- lIdx + 1L
 
-  ## respect limits
-  lIdx[which(lIdx < 1L)] <- 1L
-  rIdx[which(rIdx > length(vec))] <- length(vec)
-
   ## calculate differences for left and right
   lDiff <- abs(vec[lIdx] - x)
   rDiff <- abs(vec[rIdx] - x)
 
-  ifelse(rDiff == pmin.int(lDiff, rDiff), rIdx, lIdx)
+  if (!is.infinite(tolerance)) {
+    lIdx[lDiff > tolerance] <- nomatch
+    rIdx[rDiff > tolerance] <- nomatch
+  }
+
+  d <- which(lDiff != pmin.int(lDiff, rDiff))
+  lIdx[d] <- rIdx[d]
+  lIdx
 }
--- tests/testthat/test_which.closest-functions.R
+++ tests/testthat/test_which.closest-functions.R
@@ -6,5 +6,11 @@ test_that(".which.closest", {
   expect_equal(MALDIquant:::.which.closest(9.8, 1:10), 10)
   expect_equal(MALDIquant:::.which.closest(11.1, 1:10), 10)
   expect_equal(MALDIquant:::.which.closest(c(0.5, 1.5, exp(1), pi), 1:10),
-               c(1, 2, 3, 3))
+               c(1, 1, 3, 3))
+})
+
+test_that(".which.closest, tolerance", {
+  expect_equal(MALDIquant:::.which.closest(1.001, 1:10, tolerance=0), NA_integer_)
+  expect_equal(MALDIquant:::.which.closest(1.001, 1:10, tolerance=0, nomatch=-1), -1)
+  expect_equal(MALDIquant:::.which.closest(1.4, 1:10, tolerance=0.4), 1)
 })
