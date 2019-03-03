15a2619d2ff01fa1f184946e3c105351a53f380a
Sebastian Gibb <mail@sebastiangibb.de>
2019-03-03 22:47:14 +0100

    First draft implementation of OnDiskVector

---
+++ R/OnDiskVector-class.R
@@ -0,0 +1,141 @@
+#' @include hidden_aliases.R
+NULL
+
+#' @title OnDiskVector class
+#'
+#' @name OnDiskVector
+#'
+#' @aliases OnDiskVector-class
+#'
+#' @description
+#'
+#' [OnDiskVector-class] objects support the storage of numeric data on-disk. The
+#' data are just loaded into memory when they have to be processed.
+#'
+#' @slot path file path
+#' @slot n length of the vector
+#' @slot offset offset of the data in the file
+#' @slot size size of one vector element in the file
+#'
+#' @author Sebastian Gibb <mail@@sebastiangibb.de>
+#'
+#' @noRd
+setClass("OnDiskVector",
+    slots=list(
+        path="character",
+        n="numeric",
+        offset="numeric",
+        size="integer"
+    ),
+    prototype=list(
+        path=character(), n=numeric(), offset=numeric(), size=integer()
+    )
+)
+
+OnDiskVector <- function(x, path, n=length(x), offset=0L, size=8L) {
+    if (missing(x) && missing(path))
+        stop("'x' or 'path' are necessary.")
+    if (!missing(x)) {
+        if (missing(path))
+            path <- tempfile()
+        writeBin(as.double(x), con=path, size=size, endian="little")
+    }
+    new("OnDiskVector", path=path, n=n, offset=offset, size=size)
+}
+
+.valid.OnDiskVector.path <- function(x) {
+    if (length(x) != 1L)
+        return("'path' has to be a 'character' of length 1.")
+    if (!file.exists(x))
+        return(paste0("File '", x, "' doesn't exists!"))
+    NULL
+}
+
+.valid.OnDiskVector.n <- function(x) {
+    if (length(x) != 1L)
+        return("'n' has to be a 'numeric' of length 1.")
+    NULL
+}
+
+.valid.OnDiskVector.offset <- function(x) {
+    if (length(x) != 1L)
+        return("'offset' has to be a 'numeric' of length 1.")
+    if (x < 0)
+        return("'offset' has to be >= 0.")
+    NULL
+}
+
+.valid.OnDiskVector.size <- function(x) {
+    if (length(x) != 1L)
+        return("'size' has to be a 'integer' of length 1.")
+    if (log2(x) %% 1)
+        return("'size' has to be 2^x.")
+    NULL
+}
+
+setValidity("OnDiskVector", function(object) {
+    msg <- c(
+        .valid.OnDiskVector.path(object@path),
+        .valid.OnDiskVector.n(object@n),
+        .valid.OnDiskVector.offset(object@offset),
+        .valid.OnDiskVector.size(object@size)
+    )
+    if (is.null(msg)) { TRUE } else { msg }
+})
+
+#' @rdname hidden_aliases
+setMethod("length", "OnDiskVector", function(x)x@n)
+
+#' @rdname hidden_aliases
+setMethod(f="[",
+    signature=signature(x="OnDiskVector", i="numeric", j="missing"),
+    definition=function(x, i, j, ..., drop=FALSE) {
+    if (any(i < 1) || any (i > x@n))
+        stop("Index out of boundaries.")
+
+    f <- file(x@path, "rb")
+    on.exit(close(f))
+
+    if (length(i) == 1L) {
+        if (x@offset || i > 1L)
+            seek(f, where=x@offset + (i - 1L) * x@size, rw="read")
+        .readBin(f, n=1L, size=x@size)
+    } else if (length(i) == 2) {
+        if (x@offset || i > 1L)
+            seek(f, where=x@offset + (i[1L] - 1L) * x@size, rw="read")
+        y <- .readBin(f, n=1L, size=x@size)
+            seek(f, where=x@offset + (i[2L] - 1L) * x@size, rw="read")
+        c(y, .readBin(f, n=1L, size=x@size))
+    } else {
+        if (x@offset)
+            seek(f, where=x@offset, rw="read")
+        # that's stupid but not used often
+        .readBin(f, n=x@n, size=x@size)[i]
+    }
+})
+
+#' @rdname hidden_aliases
+setMethod(f="[",
+    signature=signature(x="OnDiskVector", i="missing", j="missing"),
+    definition=function(x, i, j, ..., drop=FALSE) {
+    f <- file(x@path, "rb")
+    on.exit(close(f))
+    if (x@offset)
+        seek(f, where=x@offset, rw="read")
+    .readBin(f, n=x@n, size=x@size)
+})
+
+#' @rdname hidden_aliases
+setReplaceMethod(f="[",
+    signature=signature(x="OnDiskVector", i="missing", j="missing"),
+    definition=function(x, i, j, ..., value) {
+    if (length(value) != x@n) {
+        stop("Length of 'value' doesn't match length of 'x'.")
+    }
+    writeBin(as.double(value), x@path, size=x@size, endian="little")
+    x
+})
+
+.readBin <- function(x, n, size) {
+    readBin(x, double(), n=n, size=size, signed=TRUE, endian="little")
+}
---
+++ tests/testthat/test_OnDiskVector-class.R
@@ -0,0 +1,67 @@
+context("OnDiskVector")
+
+test_that(".valid.OnDiskVector.path", {
+    f <- tempfile()
+    on.exit(unlink(f))
+    file.create(f)
+    expect_null(.valid.OnDiskVector.path(f))
+    expect_match(.valid.OnDiskVector.path(1:2), "length 1")
+    expect_match(.valid.OnDiskVector.path(tempfile()), "exists")
+})
+
+test_that(".valid.OnDiskVector.n", {
+    expect_null(.valid.OnDiskVector.n(1))
+    expect_match(.valid.OnDiskVector.n(1:2), "length 1")
+})
+
+test_that(".valid.OnDiskVector.size", {
+    expect_null(.valid.OnDiskVector.size(1))
+    expect_null(.valid.OnDiskVector.size(8))
+    expect_match(.valid.OnDiskVector.size(1:2), "length 1")
+    expect_match(.valid.OnDiskVector.size(3), "has to be 2\\^x")
+})
+
+test_that(".valid.OnDiskVector.n", {
+    expect_null(.valid.OnDiskVector.offset(1))
+    expect_match(.valid.OnDiskVector.offset(1:2), "length 1")
+    expect_match(.valid.OnDiskVector.offset(-1), ">= 0")
+})
+
+test_that("validity", {
+    odv <- OnDiskVector(1:3)
+    f <- odv@path
+    odv@path <- "foobar"
+    expect_error(validObject(odv), "exists")
+    odv@path <- f
+    odv@offset <- -1
+    expect_error(validObject(odv), ">= 0")
+})
+
+test_that("constructor", {
+    expect_error(OnDiskVector(), "necessary")
+})
+
+test_that("length", {
+    odv <- OnDiskVector(1:10)
+    expect_length(odv, 10)
+})
+
+test_that("[", {
+    odv <- OnDiskVector(1:10)
+    expect_error(odv[0], "out of boundaries")
+    expect_error(odv[11], "out of boundaries")
+    expect_equal(odv[3], 3)
+    expect_equal(odv[c(1, 10)], c(1, 10))
+    expect_equal(odv[c(1, 2, 10)], c(1, 2, 10))
+    expect_equal(odv[], 1:10)
+    expect_equal((odv[] <- 11:20)[1:10], 11:20)
+
+    fn <- tempfile()
+    f <- file(fn, "wb")
+    writeBin(as.double(1:20), f, size=4L, endian="little")
+    close(f)
+    on.exit(unlink(fn))
+
+    odv2 <- OnDiskVector(path=fn, offset=4L * 10L, n=10L, size=4L)
+    expect_equal(odv2[1:3], 11:13)
+})
